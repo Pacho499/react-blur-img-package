a3e04908db5886c069bda41302eb6422e8213359
Lorenzo Palumbo <lorenzopalumbo.dev@gmail.com>
2024-09-16 15:22:18 +0200

    feat: new props to style border

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./app.css";
+
+export interface SpinnerProps {
+  width: React.CSSProperties["width"];
+  height: React.CSSProperties["height"];
+  border?: React.CSSProperties["border"];
+  borderTop?: React.CSSProperties["borderTop"];
+  color?: React.CSSProperties["borderColor"];
+  style?: React.CSSProperties["borderStyle"];
+}
 
 interface Props {
   img: string;
-  imgSm: string;
+  placeHolder: string;
   alt: string;
+  aspectRatio?: string;
+  objectFit?: React.CSSProperties["objectFit"];
+  objectPosition?: React.CSSProperties["objectPosition"];
+  spinner?: SpinnerProps;
 }
 
-function App({ img, imgSm, alt }: Props) {
+function BlurImg({
+  img,
+  placeHolder,
+  alt,
+  aspectRatio,
+  objectFit,
+  objectPosition,
+  spinner,
+}: Props) {
   const blurLoadDiv = useRef<HTMLDivElement>(null);
   const image = useRef<HTMLImageElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const loaded = (): void => {
     if (blurLoadDiv.current) {
+      setLoading(false);
       blurLoadDiv.current.classList.add("loaded");
     }
   };
@@ -31,8 +54,18 @@ function App({ img, imgSm, alt }: Props) {
     <div
       className="blur-load-container"
       ref={blurLoadDiv}
-      style={{ backgroundImage: `url(${imgSm})` }}
+      style={
+        {
+          backgroundImage: `url(${placeHolder})`,
+          "--aspect-ratio": aspectRatio,
+          "--object-fit": objectFit,
+          "--object-position": objectPosition,
+          "--border-color": spinner?.color,
+          "--border-style": spinner?.style,
+        } as React.CSSProperties
+      }
     >
+      {loading && <div className="spinner" style={spinner}></div>}
       <img
         className="blur-load-img"
         src={img}
@@ -44,4 +77,4 @@ function App({ img, imgSm, alt }: Props) {
   );
 }
 
-export default App;
+export { BlurImg };
